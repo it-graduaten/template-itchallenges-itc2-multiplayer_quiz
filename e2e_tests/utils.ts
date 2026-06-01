dbf873da9b2e57358da8edbc6dcec572cd27a38e
Joren Synaeve - Thomas More <111964430+JorenSynaeveTM@users.noreply.github.com>
2026-06-01 10:40:21 +0200

    Refactor utils.ts for improved structure and imports

--- e2e_tests/utils.ts
+++ e2e_tests/utils.ts
@@ -1,4 +1,5 @@
-import {expect, Page} from "@playwright/test";
+import {expect} from "@playwright/test";
+import type {Page} from "@playwright/test";
 
 export const completeHomePage = async (page: Page, amountOfPlayers: number = 1, amountOfQuestions: number = 1, typeOfQuestions: string = 'custom') => {
     const inputQuestionAmount = page.getByTestId('input-question-amount');
